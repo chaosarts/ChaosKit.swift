e6f75e3aef112338a5b21939ea4d9ede6945e30c
Fu Lam Diep <fulam.diep@chaosarts.de>
2015-05-05 21:40:05 +0200

    [Add] ElementBuffer class added (no implementation yet)

---
+++ ChaosKit/GLVertexElementBuffer.swift
@@ -0,0 +1,21 @@
+//
+//  GLVertexElementBuffer.swift
+//  ChaosKit
+//
+//  Created by Fu Lam Diep on 03.05.15.
+//  Copyright (c) 2015 Fu Lam Diep. All rights reserved.
+//
+
+import Foundation
+
+
+public class GLVertexElementBuffer : GLVertexBufferBase, GLVertexBuffer {
+	
+	public init () {
+		super.init (target: GL_ELEMENT_ARRAY_BUFFER)
+	}
+	
+	public func draw () {
+		
+	}
+}
