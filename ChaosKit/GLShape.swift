bcba5f4480ca650d7a687add1e7caf5a1d8cebcf
Fu Lam Diep <fulam.diep@chaosarts.de>
2015-05-08 19:51:38 +0200

    [Update] Complete refactoring of the shape class

--- ChaosKit/GLShape.swift
+++ ChaosKit/GLShape.swift
@@ -16,94 +16,238 @@ in corresponding GLVertexAttribute objects.
 */
 public class GLShape : GLDisplayObject {
 	
-	// STORED VALUES
-	// *************
+	/*
+	|--------------------------------------------------------------------------
+	| Stored properties
+	|--------------------------------------------------------------------------
+	*/
 	
-	/// Provides a attribute-alias-attribute-map
 	private var _attributes : [GLAttribAlias : GLVertexAttribute] = [GLAttribAlias : GLVertexAttribute]()
+	
+	/// Provides a list of indices for GL_ELEMENT_ARRAY_BUFFER
+	private var _indexlist : [Int]?
 	
 	/// indexWrapper functions for the attributes values in case
 	/// their values count won't match the position count
 	private var _indexWrappers : [GLAttribAlias : CKIndexWrapper] = [GLAttribAlias : CKIndexWrapper]()
+		
+	/// Provides the vertex buffer object for this shape
+	private var _vertexBuffer : GLVertexBuffer
 	
-	/// Provides the color to use for next vertex, if provided
-	private var _color : vec4?
+	/// Indicates if the shape is dirty according to the vertex buffer
+	private var _setup : Bool = false
 	
-	/// Provides the normal to use for next vertex, if provided
-	private var _normal : vec3?
-
-	/// The draw mode of the shape
-	public var mode : GLenum = GLenum(GL_TRIANGLES)
+	/// Indicates if the shape has been compile or not
+	private var _compiled : Bool = false
 	
 	
-	// DERIVED VALUES
-	// **************
+	/*
+	|--------------------------------------------------------------------------
+	| Derived properties
+	|--------------------------------------------------------------------------
+	*/
+	
+	
+	/// Contains the count of vertice of this shape
+	public var count : Int {
+		get {return _attributes[.Position]!.count}
+	}
 	
 	/// Provides the color to use for next vertex
-	public var color : vec4? {
-		get {return _color}
-		set {if !canSetForAlias(.Color) || newValue == nil {return}}
-	}
+	public var color : vec4?
 	
 	/// Provides the normal to use for next vertex, if provided
-	public var normal : vec3? {
-		get {return _normal}
-		set {if !canSetForAlias(.Normal) && newValue != nil {_normal = newValue}}
+	public var normal : vec3?
+	
+	/// Provides the vertex buffer object for this shape
+	public var vertexBuffer : GLVertexBuffer {
+		get {compile(); return _vertexBuffer}
 	}
 	
-	// SUBSCRIPTS 
-	// **********
+	/// Provides the draw mode
+	public var mode : GLenum {
+		get {return _vertexBuffer.mode}
+		set {_vertexBuffer.mode = newValue}
+	}
+	
+	/// Provides a map of vertex attribute values, where key is the
+	/// targeted attribute and value the vertex attribute values
+	public var attributes : [GLAttribAlias : GLVertexAttribute] {get {return _attributes}}
+	
+	
+	/*
+	|--------------------------------------------------------------------------
+	| Subscripts
+	|--------------------------------------------------------------------------
+	*/
+	
+	/**
+	Returns the according vertex attribute values as GLVertexAttribute object
+	
+	:param: attribute An attribute alias
+	*/
+	public subscript (attribute: GLAttribAlias) -> GLVertexAttribute? {
+		get {return _attributes[attribute]}
+	}
+	
+	
+	/**
+	Returns one attribute value of one vertex
+	
+	:param: alias An vertex attribute alias
+	:param: index The index of the vertex to get the attribute value
+	*/
+	public subscript (alias: GLAttribAlias, index: Int) -> [GLfloat] {
+		get {
+			var attribute : GLVertexAttribute? = (self)[alias]
+			if attribute == nil || attribute!.count == 0 {return []}
+			
+			var newIndex : Int = index
+			if alias != .Position {
+				newIndex = _indexWrappers[alias]!(val: index, minVal: 0, maxVal: attribute!.count)
+			}
+			return attribute![newIndex]
+		}
+	}
 
-	/** Initialzes the Shape */
-	public override init () {
-		super.init()
+	
+	/*
+	|--------------------------------------------------------------------------
+	| Initializers
+	|--------------------------------------------------------------------------
+	*/
+	
+	/** 
+	Initialzes the Shape 
+	*/
+	public convenience override init () {
+		self.init(useIndex: false)
+	}
+	
+	
+	public init (useIndex: Bool) {
+		
 		_attributes[.Position] = GLVertexAttributeArray<vec3>()
-		
 		_attributes[.Color] = GLVertexAttributeArray<vec4>()
-		_indexWrappers[.Color] = repeatIndex
-		
 		_attributes[.Normal] = GLVertexAttributeArray<vec3>()
+		
+		_indexWrappers[.Color] = repeatIndex
 		_indexWrappers[.Normal] = repeatIndex
+		
+		if useIndex {_vertexBuffer = GLVertexElementBuffer()}
+		else {_vertexBuffer = GLVertexArrayBuffer()}
 	}
 	
 	
+	/*
+	|--------------------------------------------------------------------------
+	| Methods
+	|--------------------------------------------------------------------------
+	*/
+	
+	
+	public func draw (program: GLProgram) {
+		for buffer in vertexBuffer.buffers {
+			buffer.bind()
+			for block in buffer.blocks {
+				var attribute : GLAttribLocation? = program.getAttribLocation(block.attribute)
+				attribute?.enable()
+				attribute?.setVertexAttribPointer(block)
+			}
+			
+			glDrawArrays(mode, 0, GLsizei(count))
+		}
+	}
+	
+	/**
+	Creates a vertex with given position and set color and normal if set
+	
+	:param: position The position where the vertex will be set
+ 	*/
 	public func createVertex (position: vec3) {
+		
+		if canSetForAlias(.Color) && color != nil {
+			_attributes[.Color]!.append(color!.array)
+		}
+		
+		if canSetForAlias(.Normal) && normal != nil {
+			_attributes[.Normal]!.append(normal!.array)
+		}
+		
 		_attributes[.Position]!.append(position.array)
-		
-		if canSetForAlias(.Color) && _color != nil {
-			_attributes[.Color]!.append(_color!.array)
-		}
-		
-		if canSetForAlias(.Normal) && _normal != nil {
-			_attributes[.Normal]!.append(_normal!.array)
-		}
+		_compiled = false
 	}
 	
 	
-	public func getColor (index: Int) -> [GLfloat] {
-		var attribute : GLVertexAttribute = _attributes[.Color]!
-		if attribute.count == 0 {return []}
-
-		var wrapper : CKIndexWrapper = _indexWrappers[.Color]!
-		var newindex : Int = wrapper(index: index, min: 0, max: attribute.count - 1)
-		
-		return attribute[newindex]
+	public func compile () {
+		if _compiled {return}
+		setupBuffer()
+		_vertexBuffer.buffer(self)
 	}
 	
 	
-	public func getNormal (index: Int) -> [GLfloat] {
-		var attribute : GLVertexAttribute = _attributes[.Normal]!
-		if attribute.count == 0 {return []}
+	private func setupBuffer () {
 		
-		var wrapper : CKIndexWrapper = _indexWrappers[.Normal]!
-		var newindex : Int = wrapper(index: index, min: 0, max: attribute.count - 1)
+		if _setup {return}
 		
-		return attribute[newindex]
+		// Group by dynmaic and static attributes
+		// **************************************
+		
+		// Assign all attribute values to dynamic group and filter one by one to static
+		var dynamicAttributes : [GLAttribAlias : GLVertexAttribute] = _attributes
+		var staticAttributes : [GLAttribAlias : GLVertexAttribute] = [GLAttribAlias : GLVertexAttribute]()
+		
+		// Memorize the stride for static attributes
+		var stride : Int = 0
+		var buffers : [GLBuffer] = []
+		
+		// This loop iterates through the dynamic attribute group, configures
+		// the buffers and moves the static attributes to a separate group
+		for key in dynamicAttributes.keys {
+			var attribute : GLVertexAttribute = dynamicAttributes[key]!
+			
+			// Handle only non-dynmaic buffers
+			if attribute.dynamic {
+				/// Creates one buffer per dynamic
+				var block : GLBufferBlock = GLBufferBlock(key, attribute.size, GL_FLOAT, true, 0, 0)
+				var buffer : GLBuffer = GLBuffer(target: _vertexBuffer.target, usage: GLenum(GL_DYNAMIC_DRAW), blocks: [block])
+				buffers.append(buffer)
+				continue
+			}
+			
+			// Do not add empty attributes
+			if attribute.count == 0 {continue}
+			
+			// Append attribute to static group and remove it from dynamic
+			staticAttributes[key] = attribute
+			dynamicAttributes.removeValueForKey(key)
+			
+			// Increase stride with new appended static attribute
+			stride += attribute.size
+		}
+		
+		// Configure the static buffer
+		var offset : Int = 0
+		var sBlocks : [GLBufferBlock] = []
+		for key in staticAttributes.keys {
+			var attribute : GLVertexAttribute = staticAttributes[key]!
+			var block : GLBufferBlock = GLBufferBlock(key, attribute.size, GL_FLOAT, true, stride, offset)
+			sBlocks.append(block)
+			offset += Int(block.size)
+		}
+		
+		buffers.append(GLBuffer(target: _vertexBuffer.target, usage: GLenum(GL_STATIC_DRAW), blocks: sBlocks))
+		_vertexBuffer.buffers = buffers
+		
+		_setup = true
 	}
 	
 	
 	private func canSetForAlias (alias: GLAttribAlias) -> Bool {
-		return _attributes[alias] != nil && _attributes[alias]!.count == _attributes[.Position]!.count
+		var attribute : GLVertexAttribute? = _attributes[alias]
+		var isset : Bool = attribute != nil
+		var sameCount : Bool = attribute!.count == self.count
+		return isset && sameCount
 	}
 }
 
