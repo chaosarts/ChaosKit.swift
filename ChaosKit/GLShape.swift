cfbcf749dfbf73c544ca7fd747eff06d308cb9b5
Fu Lam Diep <fulam.diep@chaosarts.de>
2015-06-12 11:41:28 +0200

    REMOVE shape buffer is not needed. buffer configuration moved to shape

--- ChaosKit/GLShape.swift
+++ ChaosKit/GLShape.swift
@@ -17,25 +17,26 @@ in corresponding GLAttribData objects.
 @objc
 public class GLShape : GLDisplayObject {
 	
-	/*
-	|--------------------------------------------------------------------------
-	| Stored properties
-	|--------------------------------------------------------------------------
-	*/
+	// STORED PROPERTIES
+	// +++++++++++++++++
 	
-	private var _buffer : GLShapeBuffer?
+	/// Provides a list of buffer objects, which are to use when configuring this vertex array
+	private var _buffers : [GLBuffer]?
+	
+	/// Indicates whether the shape is compiled or not
+	private var _compiled : Bool = false
+	
+	/// Provides the target
+	public private(set) var target : GLBufferTarget = GLArrayBufferTarget()
 
 	/// Provides the geometry
-	public var geometry : GLShapeProperty
+	public var geometry : GLGeometry {
+		willSet {if geometry.dynamic != newValue.dynamic {_buffers = nil}}
+		didSet {_compiled = false}
+	}
 	
 	/// Provides the surface
 	public var surface : GLSurface = GLSurface()
-	
-	/*
-	|--------------------------------------------------------------------------
-	| Derived properties
-	|--------------------------------------------------------------------------
-	*/
 	
 	/// Provides the line width
 	public var linewidth : GLfloat = 1
@@ -45,6 +46,10 @@ public class GLShape : GLDisplayObject {
 	
 	/// Provides the draw mode
 	public var mode : GLenum = GLenum(GL_TRIANGLES)
+	
+	
+	// DERIVED PROPERTIES
+	// ++++++++++++++++++
 	
 	/// Provides the bufferables
 	public var bufferables : [GLLocationSelector : GLBufferable] {
@@ -57,37 +62,109 @@ public class GLShape : GLDisplayObject {
 	}
 	
 	
-	public var buffer : GLShapeBuffer {get {compile(); return _buffer!}}
+	public var uniforms : [GLLocationSelector : GLUniform] {
+		get {
+			var uniforms : [GLLocationSelector : GLUniform] = surface.uniforms
+			uniforms[GLLocationSelector(type: .ModelViewMatrix)] = GLUniformMatrix4fv(modelViewMatrix)
+			uniforms[GLLocationSelector(type: .ProjectionViewMatrix)] = GLUniformMatrix4fv(modelViewMatrix)
+			return uniforms
+		}
+	}
+	
+	
+	/// Provides the shape in the compiled version ready to upload to a program
+	public var buffers : [GLBuffer] {
+		get {
+			_configureBuffers()
+			
+			if _compiled {return _buffers!}
+			
+			for buffer in _buffers! {
+				var data : [GLfloat] = []
+				
+				for index in 0..<geometry.count {
+					for block in buffer.blocks {
+						var bufferable : GLBufferable = bufferables[block.selector]!
+						data += bufferable[index]
+					}
+				}
+				
+				buffer.bind()
+				buffer.buffer(data)
+			}
+			
+			_compiled = true
+			
+			return _buffers!
+		}
+	}
 
 	
-	/*
-	|--------------------------------------------------------------------------
-	| Initializers
-	|--------------------------------------------------------------------------
-	*/
+	// INITIALIZERS
+	// ++++++++++++
 	
 	/** 
 	Initialzes the Shape 
 	*/
-	public init (geometry: GLShapeProperty) {
+	public init (geometry: GLGeometry) {
 		self.geometry = geometry
 	}
 	
 	
-	/*
-	|--------------------------------------------------------------------------
-	| Methods
-	|--------------------------------------------------------------------------
-	*/
+	// METHODS
+	// +++++++
 	
-	public func compile () {
-		if _buffer != nil {return}
-		var target : GLBufferTarget
+	/**
+	Configures the buffers
+	*/
+	private func _configureBuffers () {
+		if nil != _buffers {return}
 		
-		if geometry.indexed {target = GLArrayBufferTarget()}
-		else {target = GLElementBufferTarget()}
+		// Group by dynmaic and static attributes
+		// **************************************
 		
-		_buffer = GLShapeBuffer(target: target)
-		_buffer!.buffer(self)
+		_buffers = []
+		
+		// Store static bufferables in this array to configure static buffer later
+		var staticBufferables : [GLLocationSelector : GLBufferable] = [GLLocationSelector : GLBufferable]()
+		
+		// Store the stride
+		var stride : Int = 0
+		
+		// Configure dynamic buffers and preconfigure static buffer
+		for selector in bufferables.keys {
+			
+			var bufferable : GLBufferable = bufferables[selector]!
+			
+			// Handle only non-dynmaic buffers
+			if bufferable.dynamic {
+				/// Creates one buffer per dynamic
+				var block : GLBufferBlock = GLBufferBlock(selector, bufferable.size, GL_FLOAT, true, 0, 0)
+				var buffer : GLBuffer = GLBuffer(target: target.value, usage: GLenum(GL_DYNAMIC_DRAW), blocks: [block])
+				_buffers!.append(buffer)
+				continue
+			}
+			
+			// Append buffer to static group
+			staticBufferables[selector] = bufferable
+			
+			// Increase stride with new appended static attribute
+			stride += bufferable.size
+		}
+		
+		
+		// Configure the static buffer
+		var offset : Int = 0
+		var blocks : [GLBufferBlock] = []
+		for selector in staticBufferables.keys {
+			var bufferable : GLBufferable = bufferables[selector]!
+			var block : GLBufferBlock = GLBufferBlock(selector, bufferable.size, GL_FLOAT, true, stride, offset)
+			blocks.append(block)
+			offset += Int(block.size)
+		}
+		
+		_buffers!.append(GLBuffer(target: target.value, usage: GLenum(GL_STATIC_DRAW), blocks: blocks))
+		target = geometry.indexed ? GLElementBufferTarget(indices: geometry.indexlist!) : GLArrayBufferTarget()
+		_compiled = false
 	}
 }
