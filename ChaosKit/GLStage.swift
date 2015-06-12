0c294b2a56d3ab9ec80fcdbb883b646bbb2de114
Fu Lam Diep <fulam.diep@chaosarts.de>
2015-06-12 11:43:13 +0200

    [Update] Comments added

--- ChaosKit/GLStage.swift
+++ ChaosKit/GLStage.swift
@@ -8,19 +8,41 @@
 
 import Cocoa
 
-@objc
-public class GLStage : GLContainer {
+/** 
+Stage class to place display objects
+*/
+@objc public class GLStage : GLContainer {
 	
+	// INITIALIZERS
+	// ++++++++++++
+	
+	/**
+	Initializes the stage
+	*/
 	public override init () {
 		super.init()
 	}
 	
+	
+	// METHODS
+	// ++++++
+	
+	/**
+	Adds a display object to this stage
+	
+	:param: child The object to add
+	*/
 	public override func addChild(child: GLDisplayObject) {
 		super.addChild(child)
 		child.stage = self
 	}
 	
 	
+	/**
+	Removes a child from this stage
+	
+	:param: child The child to remove
+	*/
 	public override func removeChild(child: GLDisplayObject) -> Int? {
 		var index : Int? = super.removeChild(child)
 		child.stage = nil
@@ -28,6 +50,11 @@ public class GLStage : GLContainer {
 	}
 	
 	
+	/**
+	Removes a child at given index from this stage
+	
+	:param: index The index of the child to remove
+	*/
 	public override func removeChildAt(index: Int) -> GLDisplayObject? {
 		var child = super.removeChildAt(index)
 		child?.stage = nil
