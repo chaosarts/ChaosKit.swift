ec075817b71f8e402ba016c623c75e8627daec0e
Fu Lam Diep <fulam.diep@chaosarts.de>
2015-06-18 11:16:30 +0200

    [Update] Common Math file updated

--- ChaosKit/Math.swift
+++ ChaosKit/Math.swift
@@ -70,6 +70,166 @@ public func rad2deg (value: Double) -> Double {
 	return 180 / M_PI * value
 }
 
+
+// VECTOR ----------------------------------------------------------
+
+/**
+Negates the vector
+
+:param: x The vector to negate
+:return: The negated vector
+*/
+public prefix func -<T: Vector> (x: T) -> T {
+	var array : [GLfloat] = []
+	
+	let xArray : [GLfloat] = x.array
+	for index in 0..<T.elementCount {
+		array.append(-xArray[index])
+	}
+	
+	return T(array)
+}
+
+
+/**
+Adds two vectors
+
+:param: x The first vector of the sum
+:param: y The second vector of the sum
+:return: The sum of both vectors
+*/
+public func +<T: Vector> (x: T, y: T) -> T {
+	var array : [GLfloat] = []
+	
+	let xArray : [GLfloat] = x.array
+	let yArray : [GLfloat] = y.array
+	
+	for index in 0..<T.elementCount {
+		array.append(xArray[index] + yArray[index])
+	}
+	
+	return T(array)
+}
+
+
+/**
+Subtract two vectors
+
+:param: x The first vector of the diff
+:param: y The second vector of the diff
+:return: The diff of both vectors
+*/
+public func +=<T: Vector> (x: T, y: T) -> T {
+	return x + y
+}
+
+
+/**
+Subtract two vectors
+
+:param: x The first vector of the diff
+:param: y The second vector of the diff
+:return: The diff of both vectors
+*/
+public func -<T: Vector> (x: T, y: T) -> T {
+	return x + -y
+}
+
+
+/**
+Subtract two vectors
+
+:param: x The first vector of the diff
+:param: y The second vector of the diff
+:return: The diff of both vectors
+*/
+public func -=<T: Vector> (x: T, y: T) -> T {
+	return x - y
+}
+
+
+/**
+Multiplies two vectors
+
+:param: x The first vector of the product
+:param: y The second vector of the product
+:return: The component wise product of both vectors
+*/
+public func *<T: Vector> (x: T, y: T) -> T {
+	var array : [GLfloat] = []
+	
+	let xArray : [GLfloat] = x.array
+	let yArray : [GLfloat] = y.array
+	
+	for index in 0..<T.elementCount {
+		array.append(xArray[index] * yArray[index])
+	}
+	
+	return T(array)
+}
+
+
+/**
+Multiplies a vector with a scalar
+
+:param: x The vector of the product
+:param: y The scalar of the product
+:return: The component wise product
+*/
+public func *<T: Vector> (x: T, y: GLfloat) -> T {
+	var array : [GLfloat] = []
+	
+	let xArray : [GLfloat] = x.array
+	
+	for index in 0..<T.elementCount {
+		array.append(xArray[index] * y)
+	}
+	
+	return T(array)
+}
+
+
+/**
+Multiplies a vector with a scalar
+
+:param: x The vector of the product
+:param: y The scalar of the product
+:return: The component wise product
+*/
+public func *<T: Vector> (x: GLfloat, y: T) -> T {
+	return y * x
+}
+
+
+/**
+The dot product of two vectors
+*/
+public func dot<T: Vector> (l: T, r: T) -> GLfloat {
+	var product : GLfloat = 0
+	
+	for i in 0..<T.elementCount {
+		product += l.array[i] * r.array[i]
+	}
+	
+	return product
+}
+
+/**
+Returns a orthonormal basis from three independent vectors
+*/
+public func gramschmidt<T: Vector>(a: T, vectors: T...) -> [T] {
+	var basis : [T] = [a]
+	for index in 0..<vectors.count {
+		var w : T = vectors[index]
+		for v in basis {
+			w -= dot(v, w) / dot(v, v) * v
+		}
+		
+		basis.append(w)
+	}
+	return basis
+}
+
 /*
 |--------------------------------------------------------------------------
 | Protocols
