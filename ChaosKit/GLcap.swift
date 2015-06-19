fe4fa53c5cc214ad43263d9ed983248c1a6d4817
Fu Lam Diep <fulam.diep@chaosarts.de>
2015-06-19 15:51:02 +0200

    [Update] GLcap init process extended to various parantheses

--- ChaosKit/GLcap.swift
+++ ChaosKit/GLcap.swift
@@ -16,8 +16,18 @@ public struct GLcap {
 	/// Contains the capability type such as GL_DEPTH_TEST
 	public let type : GLenum
 	
+	/// Indicates if the capability should be en- or disabled
+	public let enabled : Bool
+	
 	/// Provides either glEnable or glDisable
 	private let _apply : (cap: GLenum) -> Void
+	
+	
+	public init (_ type: GLenum, _ enable: Bool) {
+		self.type = type
+		self.enabled = enable
+		_apply = enable ? glEnable : glDisable
+	}
 	
 	/**
 	Initializes the capability
@@ -26,8 +36,7 @@ public struct GLcap {
 	:param: enable Indicates either to enable
 	*/
 	public init (_ type: Int32, _ enable: Bool) {
-		self.type = GLenum(type)
-		_apply = enable ? glEnable : glDisable
+		self.init(GLenum(type), enable)
 	}
 	
 	/**
