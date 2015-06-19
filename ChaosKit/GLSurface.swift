19ef8ea28928ee52071f27b552234aca69ad4555
Fu Lam Diep <fulam.diep@chaosarts.de>
2015-06-19 02:16:58 +0200

    [Update] Surface is now a class

--- ChaosKit/GLSurface.swift
+++ ChaosKit/GLSurface.swift
@@ -7,18 +7,93 @@
 //
 
 import Foundation
+import OpenGL
 
 /**
 Surface struct for shapes
 */
-public struct GLSurface {
+public class GLSurface {
+	
+	/// Caches the uniforms
+	private var _uniforms : [GLurl : GLUniform]?
+	
+	/// Provides a map of url domain to surface texture
+	public var _textures : [GLUrlDomain : GLSurfaceTexture] = [GLUrlDomain : GLSurfaceTexture]()
+	
+	/// Provides the refelction factor
+	public var reflection : GLfloat = 0
 	
 	/// Provides the color of the surface
 	public var color : GLShapeProperty?
 	
+	/// Provides the surface texture for bump mapping
+	public var colormap : GLSurfaceTexture? {
+		get {return _textures[.ColorMap]}
+		set {_textures[.ColorMap] = newValue; _uniforms = nil}
+	}
+	
+	/// Provides the surface texture for bump mapping
+	public var bumpmap : GLSurfaceTexture? {
+		get {return _textures[.BumpMap]}
+		set {_textures[.BumpMap] = newValue}
+	}
+	
+	/// Provides the surface texture for bump mapping
+	public var normalmap : GLSurfaceTexture? {
+		get {return _textures[.NormalMap]}
+		set {_textures[.NormalMap] = newValue}
+	}
+	
+	/// Provides the surface texture for bump mapping
+	public var heightmap : GLSurfaceTexture? {
+		get {return _textures[.HeightMap]}
+		set {_textures[.HeightMap] = newValue}
+	}
+	
+	/// Provides the surface texture for bump mapping
+	public var displacementmap : GLSurfaceTexture? {
+		get {return _textures[.DisplacementMap]}
+		set {_textures[.DisplacementMap] = newValue}
+	}
+	
+	/// Provides the surface texture for bump mapping
+	public var glowmap : GLSurfaceTexture? {
+		get {return _textures[.GlowMap]}
+		set {_textures[.GlowMap] = newValue}
+	}
+	
+	/// Provides the surface texture for bump mapping
+	public var diffusemap : GLSurfaceTexture? {
+		get {return _textures[.DiffuseMap]}
+		set {_textures[.DiffuseMap] = newValue}
+	}
+	
+	/// Provides the surface texture for bump mapping
+	public var specularmap : GLSurfaceTexture? {
+		get {return _textures[.SpecularMap]}
+		set {_textures[.SpecularMap] = newValue}
+	}
+	
+	
 	/// Provides the uniforms to apply to program when using this surface
 	public var uniforms : [GLurl : GLUniform] {
-		get {return [GLurl : GLUniform]()}
+		get {
+			if nil == _uniforms {
+				_uniforms = [GLurl : GLUniform]()
+				var index : Int = 0
+				for domain in _textures.keys {
+					if GLint(index) > GL.MAX_COMBINED_TEXTURE_IMAGE_UNITS {
+						warn("Max combined texture units exceeded.")
+						break
+					}
+					_uniforms![GLurl(domain, .Sampler)] = GLUniform1i(GL_TEXTURE0 + index)
+					index++
+				}
+				_uniforms![GLurl(.Surface, .Reflection)] = GLUniform1f(reflection)
+			}
+			
+			return _uniforms!
+		}
 	}
 	
 	/// Provides the bufferable properties
@@ -29,4 +104,6 @@ public struct GLSurface {
 			return bufferables
 		}
 	}
+	
+	public init () {}
 }
