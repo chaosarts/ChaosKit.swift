e64e8ce18e24b1d4b65ebd0f10c03b8e1e13669c
Fu Lam Diep <fulam.diep@chaosarts.de>
2015-06-19 02:17:23 +0200

    [Update] Structs are pronounced in lowercase after namespace

--- ChaosKit/GLGeometry.swift
+++ ChaosKit/GLGeometry.swift
@@ -20,7 +20,7 @@ Protocol for shape geometry
 public protocol GLGeometry : GLShapeProperty {
 	
 	/// Indicates the count of vertices the geometry contains
-	var count : Int {get}
+	var count : Int {mutating get}
 	
 	/// Indicates wether the geometry uses shared vertices or not
 	var indexed : Bool {get}
@@ -29,113 +29,5 @@ public protocol GLGeometry : GLShapeProperty {
 	var indexlist : [Int]? {get}
 	
 	/// Provides the normals of the geometry
-	var normals : GLShapeProperty? {get}
+	var normals : GLShapeProperty? {mutating get}
 }
-
-
-/*
-|--------------------------------------------------------------------------
-| Structs
-|--------------------------------------------------------------------------
-*/
-
-/**
-A GLGeometry struct as array
-*/
-public struct GLGeometryArray<V: Vector> : GLGeometry, ArrayLiteralConvertible {
-	
-	// STORED PROPERTIES
-	// +++++++++++++++++
-	
-	/// Provides the vertex position to index
-	private var _indexMap : [String : Int]?
-	
-	/// Provides a list of positions of the geometry
-	public private(set) var values : [V] = []
-	
-	/// Indicates whether the vertices change often or not
-	public var dynamic : Bool = false
-	
-	
-	// DERIVED PROPERTIES
-	// ++++++++++++++++++
-	
-	/// Provides the size of a vertex
-	public var size : Int {get {return V.elementCount}}
-	
-	/// Indicates the count of vertices the geoemtry contains
-	public var count : Int {get {return values.count}}
-	
-	/// Indicates wether the geometry uses shared vertices or not
-	public var indexed : Bool {get {return nil != _indexMap}}
-	
-	/// Provides the index list if geometry uses shared vertices
-	public private(set) var indexlist : [Int]?
-	
-	/// Provides the normals of the geometry
-	public var normals : GLShapeProperty?
-	
-	
-	// SUBSCRIPTS
-	// ++++++++++
-	
-	///
-	public subscript () -> V {
-		get {return V()}
-		set {
-			if !indexed {values.append(newValue); return}
-			if indexlist == nil {indexlist = []}
-			
-			var index : Int? = _indexMap![newValue.description]
-			if index == nil {
-				index = count
-				_indexMap![newValue.description] = count
-				values.append(newValue)
-			}
-			
-			indexlist!.append(index!)
-		}
-	}
-	
-	
-	/// Returns the vertex at given index as array of float
-	public subscript (index: Int) -> [GLfloat] {
-		get {return values[index].array}
-	}
-	
-	
-	// INITIALIZERS
-	// ++++++++++++
-	
-	/**
-	Initializes the geoemtry with passed values
-	*/
-	public init (_ values: [V], _ indexed: Bool = false) {
-		self.values = values
-		if indexed {
-			_indexMap = [String : Int]()
-			indexlist = []
-		}
-	}
-	
-	
-	public init (_ indexed: Bool = false) {
-		self.init([], indexed)
-	}
-	
-	
-	public init(arrayLiteral elements: V...) {
-		self.init(elements)
-	}
-}
-
-
-/*
-|--------------------------------------------------------------------------
-| Typealias
-|--------------------------------------------------------------------------
-*/
-
-public typealias GLGeometry2DArray = GLGeometryArray<vec2>
-public typealias GLGeometry3DArray = GLGeometryArray<vec3>
-public typealias GLGeometry4DArray = GLGeometryArray<vec4>
