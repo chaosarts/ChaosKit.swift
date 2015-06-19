38e9d362b0b237908c6e2d0b1196f9579708c8cd
Fu Lam Diep <fulam.diep@chaosarts.de>
2015-06-19 02:13:17 +0200

    [Add] New property struct for surface color

---
+++ ChaosKit/GLcolor.swift
@@ -0,0 +1,15 @@
+//
+//  GLSurfaceColor.swift
+//  ChaosKit
+//
+//  Created by Fu Lam Diep on 08.06.15.
+//  Copyright (c) 2015 Fu Lam Diep. All rights reserved.
+//
+
+import Foundation
+
+public typealias GLrgbcolors = GLpropa<vec3>
+public typealias GLrgbacolors = GLpropa<vec4>
+
+public typealias GLrgbcolor = GLprops<vec3>
+public typealias GLrgbacolor = GLprops<vec4>
