978eb351b5a7274da126daadb61b252900661942
Fu Lam Diep <fulam.diep@chaosarts.de>
2015-06-19 02:13:48 +0200

    [Add] New normal struct

---
+++ ChaosKit/GLnormal.swift
@@ -0,0 +1,15 @@
+//
+//  GLSurfaceNormal.swift
+//  ChaosKit
+//
+//  Created by Fu Lam Diep on 07.06.15.
+//  Copyright (c) 2015 Fu Lam Diep. All rights reserved.
+//
+
+import Foundation
+
+public typealias GLnormals2 = GLpropa<vec2>
+public typealias GLnormals3 = GLpropa<vec3>
+
+public typealias GLnormal2 = GLprops<vec2>
+public typealias GLnormal3 = GLprops<vec3>
